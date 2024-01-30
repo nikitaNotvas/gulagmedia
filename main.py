ef67f92483829b9bb9b0ba3194a3a80f761ce6d3
nikitaNotvas <nvasiuta@brooklynsteamcenter.org>
2024-01-30 10:25:28 -0500

    small changes

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template,request,redirect
 import pymysql,pymysql.cursors
-from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
+#from flask_httpauth import HTTPBasicAuth
+#from werkzeug.security import generate_password_hash, check_password_hash
+#@auth.login_required
 
 
 
-conn = pymysql.connect(
-    database="nvasiuta_todos",
-    user="nvasiuta",
-    password="244805859",
-    host='10.100.33.60',
-    cursorclass=pymysql.cursors.DictCursor
-)
+#conn = pymysql.connect(
+#    database="nvasiuta_todos",
+#    user="nvasiuta",
+#    password="244805859",
+#    host='10.100.33.60',
+#    cursorclass=pymysql.cursors.DictCursor
+#)
 
 
-auth = HTTPBasicAuth()
+#auth = HTTPBasicAuth()
 app = Flask(__name__)
 
 
-@app.route('/',methods=["GET","POST"])
-@auth.login_required
+@app.route('/')
 def index():
-    return render_template("home.html.jinja",)
+    return render_template("home.html.jinja")
